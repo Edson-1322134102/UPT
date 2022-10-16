4466fdc27dc77d4d19554a8d29070b68712acfec
Edson-1322134102 <112767711+Edson-1322134102@users.noreply.github.com>
2022-10-16 11:44:32 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,47 @@
+public class HolaMundo {
+int n1 = 10;
+if (n1>6 && n1<=10) {
+        System.out.println("aprobo metamaticas con "+n1);
+    } else {
+        System.out.println("reprobo matematicas con "+n1);
+}
+    int n2 = 10;
+if (n2>6 && n2<=10) {
+        System.out.println("aprobo quimica con "+n2);
+    } else {
+        System.out.println("reprobo quimica con "+n2);
+    }
+    int n3 = 10;
+if (n3>6 && n3<=10) {
+        System.out.println("aprobo historia con "+n3);
+    } else {
+        System.out.println("reprobo historia con "+n3);
+    }
+    int n4 = 10;
+if (n4>6 && n4<=10) {
+        System.out.println("aprobo algebra con "+n4);
+    } else {
+        System.out.println("reprobo algebra con "+n4);
+    }
+    int n5 = 10;
+if (n5>6 && n5<=10) {
+        System.out.println("aprobo geografia con "+n5);
+    } else {
+        System.out.println("reprobo geografia con "+n5);
+    }
+    int n6 = 10;
+if (n6>6 && n6<=10) {
+        System.out.println("aprobo algoritmos con "+n6);
+    } else {
+        System.out.println("reprobo algoritmos con "+n6);
+    }
+    int n7 = 10;
+if (n7>6 && n7<=10) {
+        System.out.println("aprobo expresion oral con "+n7);
+    } else {
+        System.out.println("reprobo expresion oral con "+n7);
+    }
+    float total = n1+n2+n3+n4+n5+n6+n7;
+float calificacion = total/7;
+System.out.println("calificacion "+ calificacion);
+}
